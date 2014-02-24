00f643367be54b94ffd68f4457cbeae4f79c5422
Dirk Gerdes <dirk.gerdes@googlemail.com>
2014-02-24 07:58:40 +0100

    Added boost::thread

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <boost/thread.hpp>
+
 
 using namespace std;
 
-int main()
+void thread_func()
 {
 	cout << "Hello World" << endl;
 }
+
+int main()
+{
+	boost::thread t( thread_func);
+	boost::thread u( thread_func);
+	t.join();
+	u.join();
+}
+
+
+
